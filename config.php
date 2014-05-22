297756a4a47570e208a04a87e839e045baae2a4c
Carmine <carmine.ferraro1@studenti.uniparthenope.it>
2014-05-22 10:16:02 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,26 @@
+<!--    This file is part of Webdome.
+
+    Webdome is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Webdome is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Webdome.  If not, see <http://www.gnu.org/licenses/>. -->
 <?php
 $hostDB = "localhost";
-$db_user = "ms_0124000464";
-$db_psw = "13664";
-$db_name = "ms4_0124000464";
+$db_user = "user";
+$db_psw = "pswd";
+$db_name = "dbname";
 global $hostlocale;
 global $key;
 global $host;
 $host="webdome.no-ip.biz:8080"; //server in remoto
-$key=  sha1("chiavesupercifrata");
+$key=  sha1("crypkey");
 $hostlocale="http://students.uniparthenope.it/~0124000464/Webdome";
 ?>
